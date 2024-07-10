7bf9de2eb1b9020117e6d9bc226b534cb80e444a
jxxzy <marjuniatiputri@gmail.com>
2024-07-10 12:19:11 +0700

    example of text

--- training.py
+++ training.py
@@ -6,7 +6,7 @@ model_save_path = "./model"
 tokenizer = RobertaTokenizer.from_pretrained(model_save_path)
 model = RobertaForSequenceClassification.from_pretrained(model_save_path)
 
-# Contoh teks yang akan diklasifikasikan
+# Example of text to be classified
 text = "Saya sangat senang dengan layanan ini!"
 
 # Tokenisasi Teks
