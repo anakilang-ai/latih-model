f7ab5cffe977196689e27f458789b5da48ca04c5
jxxzy <marjuniatiputri@gmail.com>
2024-07-10 12:55:51 +0700

    load test

--- test1.py
+++ test1.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import csv
 
-# Load The tokenizer and model
+# Muat tokenizer dan model
 tokenizer = RobertaTokenizer.from_pretrained('roberta_model')
 model = TFRobertaForSequenceClassification.from_pretrained('roberta_model')
 
