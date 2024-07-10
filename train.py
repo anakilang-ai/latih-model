bd37db451b86cb4f10f9c2287f9d5e6c52b91fa4
jxxzy <marjuniatiputri@gmail.com>
2024-07-10 13:47:00 +0700

    preprocessing data

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ model_name = "roberta-base"
 tokenizer = RobertaTokenizer.from_pretrained(model_name)
 model = RobertaForSequenceClassification.from_pretrained(model_name, num_labels=2)
 
-#Data preprocessing
+#preprocessing data
 def preprocess_function(examples):
     inputs = tokenizer(
         examples['question'], 
