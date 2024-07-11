d090f210e275d1a80d4f29052bb96943af4d6624
GilarYa <gilarwahibul@gmail.com>
2024-07-11 17:13:44 +0700

    lupa

--- example-chat.py
+++ example-chat.py
@@ -11,17 +11,7 @@ random_answers = [
     "Ya, saya suka bermain game, terutama game strategi.",
     "Belum, saya belum makan siang.",
     "Saya suka bermain sepak bola.",
-    "Buku favorit saya adalah 'Harry Potter'.",
-    "Halo! Apa kabar?",
-    "Apa yang kamu lakukan hari ini?",
-    "Pernah nonton film baru-baru ini?",
-    "Apa makanan favoritmu?",
-    "Ceritakan tentang hobi kamu!",
-    "Apa rencana kamu akhir pekan ini?",
-    "Apakah kamu suka bermain game?",
-    "Sudah makan siang?",
-    "Kamu suka olahraga apa?",
-    "Apa buku favoritmu?"
+    "Buku favorit saya adalah 'Harry Potter'."
 ]
 
 def main():
