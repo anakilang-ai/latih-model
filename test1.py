30bd622d617870996c78e3cf51a6b637acb356f6
isnaenti <isnaenti16@gmail.com>
2024-07-25 20:52:11 +0700

    predict test model accuracy

--- test1.py
+++ test1.py
@@ -59,9 +59,12 @@ def calculate_test_accuracy(file_path):
     attention_masks_test = tf.concat(attention_masks_test, axis=0)
     labels_test = tf.constant(df_test['encoded_answer'].values)
 
-logits_test = model(input_ids_test, attention_mask=attention_masks_test).logits
-preds_test = tf.argmax(logits_test, axis=1, output_type=tf.int32)
-test_accuracy_metric.update_state(labels_test, preds_test)
+    logits_test = model(input_ids_test, attention_mask=attention_masks_test).logits
+    preds_test = tf.argmax(logits_test, axis=1, output_type=tf.int32)
+    test_accuracy_metric.update_state(labels_test, preds_test)
+
+    test_accuracy = test_accuracy_metric.result().numpy()
+    return test_accuracy
 
 test_accuracy = test_accuracy_metric.result()
 print(f"Test accuracy: {test_accuracy:.4f}")
