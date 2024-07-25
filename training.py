8421ab295014ea9fd31b94c286550d860453af53
Ratusukmakomala <ratukomala28@gmail.com>
2024-07-25 21:33:31 +0700

    Loading model and tokenizer from 'model' Folders

---
+++ training.py
@@ -0,0 +1,27 @@
+# import
+import torch
+from transformers import RobertaTokenizerS RobertaForSequenceClassification
+
+# Loading model and tokenizer from 'model' Folders
+model_save_path = "./model"
+tokenizer = RobertaTokenizer.from_pretrained(model_save_path)
+model = RobertaForSequenceClassification.from_pretrained(model_save_path)
+
+#Contoh teks yang akan diklasifikasikan
+text = "Saya sangat senang dengan layanan yang ada saat ini! "
+
+#Tokenisasi Teks
+inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
+
+#Membuat prediksi
+with torch.no_grad():
+    outputs = model(**inputs)
+    logits = outputs.logits
+
+#prediksi label (0/1)
+predicted_class = torch.argmax(logits, dim=1).item()
+
+#Menampilkan Hasil
+label_map = {0: "Negatif", 1: "Positif"}
+print(f"Teks: {text}")
+print(f"Prediksi Sentimen: {label_map[predicted_class]}")
