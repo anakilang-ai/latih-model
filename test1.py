e738b2542d3d731a7c9f8be3012f27908cbff44f
isnaenti <isnaenti16@gmail.com>
2024-07-25 20:44:23 +0700

    func to load process csv

--- test1.py
+++ test1.py
@@ -4,16 +4,20 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import csv
 
-# Load tokenizer &  model
+# Load tokenizer & model
 tokenizer = RobertaTokenizer.from_pretrained('roberta_model')
 model = TFRobertaForSequenceClassification.from_pretrained('roberta_model')
 
-# Load Encoder label
-with open('rf1.csv', 'r', encoding='utf-8') as file:
-    reader = csv.reader(file, delimiter='|')
-    filtered_rows = [row for row in reader if len(row) == 2 and row[0].strip() != "" and row[1].strip() != ""]
+# Function to load and process CSV file
+def load_and_process_csv(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file, delimiter='|')
+        filtered_rows = [row for row in reader if len(row) == 2 and row[0].strip() != "" and row[1].strip() != ""]
+    df = pd.DataFrame(filtered_rows, columns=['question', 'answer'])
+    return df
 
-df = pd.DataFrame(filtered_rows, columns=['question', 'answer'])
+# Load Encoder label
+df = load_and_process_csv('rf1.csv')
 label_encoder = LabelEncoder()
 label_encoder.fit(df['answer'])
 
