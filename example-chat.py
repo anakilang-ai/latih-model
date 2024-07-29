a37a4a9cc66b274f83815939e5f1b1da9ada5d02
isnaenti <isnaenti16@gmail.com>
2024-07-29 20:31:28 +0700

    edit random response

--- example-chat.py
+++ example-chat.py
@@ -1,17 +1,17 @@
 import random
 
-# Daftar jawaban acak
-random_answers = [
-    "Saya baik-baik saja, terima kasih!",
-    "Hari saya sangat menyenangkan!",
-    "Saya baru saja menonton film yang bagus.",
-    "Saya suka pizza, itu makanan favorit saya.",
-    "Saya hobi bermain gitar.",
-    "Akhir pekan ini saya berencana pergi hiking.",
-    "Ya, saya suka bermain game, terutama game strategi.",
-    "Belum, saya belum makan siang.",
-    "Saya suka bermain sepak bola.",
-    "Buku favorit saya adalah 'Harry Potter'."
+# List of random responses
+responses = [
+    "I'm doing well, thank you!",
+    "My day has been fantastic!",
+    "I just watched a great movie.",
+    "I love pizza, it's my favorite food.",
+    "I enjoy playing the guitar.",
+    "This weekend I plan to go hiking.",
+    "Yes, I like playing games, especially strategy games.",
+    "No, I haven't had lunch yet.",
+    "I like playing soccer.",
+    "My favorite book is 'Harry Potter'."
 ]
 
 def main():
